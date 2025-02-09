caf5fc5f8bf73788f8a96f2f04c130ff5cd809ac
Domogo <domsatrapa@gmail.com>
2025-02-09 22:53:52 +0100

    exports

--- scenes/components/movement/movement_component.gd
+++ scenes/components/movement/movement_component.gd
@@ -3,5 +3,8 @@ extends Node2D
 @export_group("Character")
 @export var character: CharacterBody2D
 
+@export_group("Tile")
+@export var tile_size:= 16
+
 func move_character(direction: Vector2) -> void:
-	character.global_position += direction * 16
+	character.global_position += direction * tile_size
